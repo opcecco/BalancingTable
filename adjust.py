288eb95ebf27169a187f6e7303120b83c591f4b3
Oliver Ceccopieri <oliverbiotech@gmail.com>
2022-05-17 18:00:22 -0400

    Quarter damage

---
+++ adjust.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+
+import numpy as np
+import pandas as pd
+
+damage_scale = 0.25
+
+df = pd.read_csv(sys.argv[1], dtype = str)
+
+df['BaseDamage'] = np.ceil(df['BaseDamage'].astype(float) * damage_scale).astype(int)
+df['ArmourDamage'] = np.ceil(df['ArmourDamage'].astype(float) * damage_scale).astype(int)
+df['ArmourPenetration'] = np.ceil(df['ArmourPenetration'].astype(float) * damage_scale).astype(int)
+df['HelmetDamage'] = np.ceil(df['HelmetDamage'].astype(float) * damage_scale).astype(int)
+df['HelmetBleed'] = np.ceil(df['HelmetBleed'].astype(float) * damage_scale).astype(int)
+
+df.to_csv('out.csv', index = False, line_terminator = '\r\n')
