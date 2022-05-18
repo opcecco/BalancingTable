0facd7cd89125cbfef0b49e81a01df93b340ab0d
Oliver Ceccopieri <oliverbiotech@gmail.com>
2022-05-18 17:40:08 -0400

    No damage test 2

--- adjust.py
+++ adjust.py
@@ -5,14 +5,18 @@ import sys
 import numpy as np
 import pandas as pd
 
-damage_scale = 0.0
+damage_scale = 0.5
 
 df = pd.read_csv(sys.argv[1], dtype = str)
 
-df['BaseDamage'] = np.ceil(df['BaseDamage'].astype(float) * damage_scale).astype(int)
-df['ArmourDamage'] = np.ceil(df['ArmourDamage'].astype(float) * damage_scale).astype(int)
-df['ArmourPenetration'] = np.ceil(df['ArmourPenetration'].astype(float) * damage_scale).astype(int)
-df['HelmetDamage'] = np.ceil(df['HelmetDamage'].astype(float) * damage_scale).astype(int)
-df['HelmetBleed'] = np.ceil(df['HelmetBleed'].astype(float) * damage_scale).astype(int)
+# df['BaseDamage'] = np.ceil(df['BaseDamage'].astype(float) * damage_scale).astype(int)
+# df['ArmourDamage'] = np.ceil(df['ArmourDamage'].astype(float) * damage_scale).astype(int)
+# df['ArmourPenetration'] = np.ceil(df['ArmourPenetration'].astype(float) * damage_scale).astype(int)
+# df['HelmetDamage'] = np.ceil(df['HelmetDamage'].astype(float) * damage_scale).astype(int)
+# df['HelmetBleed'] = np.ceil(df['HelmetBleed'].astype(float) * damage_scale).astype(int)
+
+df['FalloffModifier'] = '0'
+df['MinFalloffDist'] = '0'
+df['MaxFalloffDist'] = '0'
 
 df.to_csv('out.csv', index = False, line_terminator = '\r\n')
